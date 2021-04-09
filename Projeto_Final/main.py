807af110241b70b3db4c77147382deafc9dee604
arthuraa98 <arthurandrade.98br@gmail.com>
2021-04-09 10:17:15 -0300

    Pegando as coordenadas do robo

--- Projeto_Final/main.py
+++ Projeto_Final/main.py
@@ -2,23 +2,49 @@ import sim
 import sys
 import time
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+def to_deg(radians):
+    return radians * (180 / np.pi)
+
+
+def get_position(clientID, object_handle):
+    ret, pos = sim.simxGetObjectPosition(clientID, object_handle, -1, sim.simx_opmode_oneshot_wait)
+    if ret > 0:
+        print("Error reading object position\n")
+        return
+
+    ret, angles = sim.simxGetObjectOrientation(clientID, object_handle, -1, sim.simx_opmode_oneshot_wait)
+    if ret > 0:
+        print("Error reading object orientation\n")
+        return
+
+    # I don't need the z-axis
+    theta = angles[2]
+    pos[2] = theta
+    return pos
+
 
 def main():
     clientID = sim.simxStart('127.0.0.1', 19999, True, True, 5000, 5)  # Connect to CoppeliaSim
     if clientID != -1:
         print('Connected to remote API server')
         # Get handles for robot parts, actuators and sensors:
-        res, motorRight = sim.simxGetObjectHandle(clientID, "Pioneer_p3dx_rightMotor", sim.simx_opmode_oneshot_wait)
-        res, motorLeft = sim.simxGetObjectHandle(clientID, "Pioneer_p3dx_leftMotor", sim.simx_opmode_oneshot_wait)
+        res, motorRight = sim.simxGetObjectHandle(clientID, 'Pioneer_p3dx_rightMotor', sim.simx_opmode_oneshot_wait)
+        res, motorLeft = sim.simxGetObjectHandle(clientID, 'Pioneer_p3dx_leftMotor', sim.simx_opmode_oneshot_wait)
+        res, robot = sim.simxGetObjectHandle(clientID, 'Pioneer_p3dx', sim.simx_opmode_oneshot_wait)
     else:
         print('Failed connecting to remote API server')
         sys.exit('Could not connect')
 
-    # while sim.simxGetConnectionId(clientID) != -1:
+    while sim.simxGetConnectionId(clientID) != -1:
+        pos = get_position(clientID, robot)  # [x,y,theta] in [cm cm rad]
+        print(f'Robot position: [{pos[0]:.2f} {pos[1]:.2f} {to_deg(pos[2]):.2f}]')
+
 
 if __name__ == "__main__":
     main()
-
 
 # # Unpacks a string into an array of floats.
 # floatValues = sim.simxUnpackFloats(rangeValues)
