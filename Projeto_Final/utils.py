7f5f03bf47d1f261a9727bc7519c67e54c1d1e11
arthuraa98 <arthurandrade.98br@gmail.com>
2021-04-21 14:05:18 -0300

    add noise in the position

--- Projeto_Final/utils.py
+++ Projeto_Final/utils.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 import sim
 
 
-def getPosition(clientID, object_handle):
+def getPosition(clientID, object_handle, add_noise=False):
     ret, pos = sim.simxGetObjectPosition(clientID, object_handle, -1, sim.simx_opmode_oneshot_wait)
     if ret > 0:
         print("Error reading object position\n")
@@ -15,4 +17,8 @@ def getPosition(clientID, object_handle):
     # I don't need the z-axis
     theta = angles[2]
     pos[2] = theta
+
+    if add_noise:
+        noise = np.random.normal(0, 0.1, pos.shape)
+        pos = pos + noise
     return pos
