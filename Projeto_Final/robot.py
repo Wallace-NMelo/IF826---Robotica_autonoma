f4f35b2d09d0d5deb8f1a067bb54460011bba5c9
arthuraa98 <arthurandrade.98br@gmail.com>
2021-04-21 10:50:40 -0300

    aaa

--- Projeto_Final/robot.py
+++ Projeto_Final/robot.py
@@ -4,12 +4,22 @@ import sim
 
 
 class Robot:
-    def __init__(self, clientID):
+    def __init__(self, clientID, v0):
         motorRight = sim.simxGetObjectHandle(clientID, 'Pioneer_p3dx_rightMotor', sim.simx_opmode_oneshot_wait)[1]
         motorLeft = sim.simxGetObjectHandle(clientID, 'Pioneer_p3dx_leftMotor', sim.simx_opmode_oneshot_wait)[1]
         self.sensorHandles = np.zeros(16)
-        self.v0 = 1
+        self.v0 = v0
         self.detect = np.zeros(16)
+
+        for i in range(16):
+            returnCode, sensorHandle = sim.simxGetObjectHandle(clientID,
+                                                                        'Pioneer_p3dx_ultrasonicSensor' + str(i+1),
+                                                                        sim.simx_opmode_oneshot_wait)
+            self.sensorHandles[i] = sensorHandle
+
+            returnCode, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = sim.simxReadProximitySensor(
+                clientID, sensorHandle, sim.simx_opmode_streaming)
+
         # weighted matrix that converts the sensor inputs into motor speeds
         self.matrix = np.array([[-0.2, -0.4, -0.6, -0.8, -1, -1.2, -1.4, -1.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
                                 [-1.6, -1.4, -1.2, -1, -0.8, -0.6, -0.4, -0.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
@@ -19,27 +29,23 @@ class Robot:
 
     def breit_controller(self, clientID):
         for i in range(16):
-            returnCode, self.sensorHandles[i] = sim.simxGetObjectHandle(clientID,
-                                                                        'Pioneer_p3dx_ultrasonicSensor' + str(i),
-                                                                        sim.simx_opmode_oneshot_wait)
             returnCode, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = sim.simxReadProximitySensor(
-                clientID, self.sensorHandles[i], sim.simx_opmode_streaming)
-        for i in range(16):
-            returnCode, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = sim.simxReadProximitySensor(
-                clientID, self.sensorHandles[i], sim.simx_opmode_buffer)
+                clientID, int(self.sensorHandles[i]), sim.simx_opmode_buffer)
             distToObject = np.sqrt(detectedPoint[0] ** 2 + detectedPoint[1] ** 2 + detectedPoint[2] ** 2)
+
             if (detectionState == True) and (distToObject < self.maxDetectionRadius):
                 if distToObject < self.minDistance:
                     distToObject = self.minDistance
-                normalizedDist = (distToObject - self.minDistance) / (self.minDistance - self.maxDetectionRadius)
+                normalizedDist = (distToObject - self.minDistance) / (self.maxDetectionRadius - self.minDistance)
                 self.detect[i] = 1 - normalizedDist
             else:
                 self.detect[i] = 0
 
+        self.speedMotors[0] = self.v0
         self.speedMotors[1] = self.v0
 
         for i in range(2):
             for j in range(16):
-                self.speedMotors[i] += self.matrix[i, j] * self.detect[i]
+                self.speedMotors[i] = self.speedMotors[i] + self.matrix[i, j] * self.detect[j]
 
         return self.speedMotors
