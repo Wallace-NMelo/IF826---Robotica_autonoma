01cca51265b00a6144a299f357cd95fd8e179761
arthuraa98 <arthurandrade.98br@gmail.com>
2021-04-21 21:20:32 -0300

    add kalman filter (alpha)

--- Projeto_Final/utils.py
+++ Projeto_Final/utils.py
@@ -3,6 +3,10 @@ import numpy as np
 import sim
 
 
+def getSimTimeMs(clientID):
+    return sim.simxGetLastCmdTime(clientID)
+
+
 def getPosition(clientID, object_handle, add_noise=False):
     ret, pos = sim.simxGetObjectPosition(clientID, object_handle, -1, sim.simx_opmode_oneshot_wait)
     if ret > 0:
@@ -19,6 +23,6 @@ def getPosition(clientID, object_handle, add_noise=False):
     pos[2] = theta
 
     if add_noise:
-        noise = np.random.normal(0, 0.1, pos.shape)
+        noise = np.random.normal(0, 0.1, len(pos))
         pos = pos + noise
     return pos
